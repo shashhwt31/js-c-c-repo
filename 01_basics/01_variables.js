9b7aaff2f04c0a91a47b10c599e1fe603930dfe2
Shashwat Singh Patel <patelshashwatsingh@gmail.com>
2025-12-25 12:42:24 +0000

    let const intro

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "shashwat@cricket.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+   
+// we are not using var
+
+accountEmail = "ssp@ict.in"
+accountPassword = "01010"
+accountCity = "Bengaluru"
+
+
+
+console.log("accountId")
+console.table([accountEmail,accountPassword,accountId,accountCity])
+
+
+    
